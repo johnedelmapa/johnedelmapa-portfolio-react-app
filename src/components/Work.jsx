d0a66b930164fa077a566b372d24f94f159f7ef5
JOHNEDEL MAPA <mapajohnedel@gmail.com>
2023-05-06 15:58:04 +0800

    Push latest changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Medrocket from '../assets/medrocket.jpg';
+import BM from '../assets/bm.png';
 import Postrio from '../assets/postrio.jpg';
-import Areascouts from '../assets/areascouts.jpg';
+import Areascouts from '../assets/areascouts.png';
 import AreascoutsWeb from '../assets/areascouts1.jpg';
 import IAM from '../assets/iam.jpg';
 import Booster from '../assets/01booster.jpg';
@@ -9,6 +10,7 @@ import Educhamp from '../assets/educhamp.jpg';
 import AirbnbClone from '../assets/airbnb.jpg';
 import Relocationplus from '../assets/relocationplus.jpg';
 import Hubmind from '../assets/hubmind.jpg';
+import MirconetBD from '../assets/micronetbd.png';
 
 const Work = () => {
 return (
@@ -23,7 +25,57 @@ return (
 
     {/* Container */}
     <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-4'>
+    <div style={{ backgroundImage: `url(${MirconetBD})` }}
+        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+        {/* Hover Effects */}
+        <div className='opacity-0 group-hover:opacity-100'>
+          <span className='text-2xl font-bold text-white tracking-wider'>
+            Wordpress Web design
+          </span>
+          <div className='pt-8 text-center'>
+            <a href='https://micronetbd.org/' target='_blank'>
+              <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                Demo
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
 
+      {/* Grid Item */}
+      <div style={{ backgroundImage: `url(${Areascouts})` }}
+        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+        {/* Hover Effects */}
+        <div className='opacity-0 group-hover:opacity-100'>
+          <span className='text-2xl font-bold text-white tracking-wider'>
+            Ruby on Rails App
+          </span>
+          <div className='pt-8 text-center'>
+            <a href='https://www.basemission.com/' target='_blank'>
+              <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                Demo
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ backgroundImage: `url(${BM})` }}
+        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+        {/* Hover Effects */}
+        <div className='opacity-0 group-hover:opacity-100'>
+          <span className='text-2xl font-bold text-white tracking-wider'>
+            Ruby on Rails App
+          </span>
+          <div className='pt-8 text-center'>
+            <a href='https://app.areascouts.com/' target='_blank'>
+              <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                Demo
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
 
       {/* Grid Item */}
       <div style={{ backgroundImage: `url(${Hubmind})` }}
@@ -70,23 +122,6 @@ return (
           </span>
           <div className='pt-8 text-center'>
             <a href='https://www.postrio.io/' target='_blank'>
-              <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                Demo
-              </button>
-            </a>
-          </div>
-        </div>
-      </div>
-      {/* Grid Item */}
-      <div style={{ backgroundImage: `url(${Areascouts})` }}
-        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-        {/* Hover Effects */}
-        <div className='opacity-0 group-hover:opacity-100'>
-          <span className='text-2xl font-bold text-white tracking-wider'>
-            Ruby on Rails App
-          </span>
-          <div className='pt-8 text-center'>
-            <a href='https://app.areascouts.com/' target='_blank'>
               <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                 Demo
               </button>
